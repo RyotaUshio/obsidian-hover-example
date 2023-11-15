f6888698d7bbad23f1b3b38763883894ba34635a
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-11-15 20:47:36 +0900

    forgot to add main.ts

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { MarkdownView, Plugin } from 'obsidian';
+
+export default class MyPlugin extends Plugin {
+	async onload() {
+		// To see what happens when you hover over a link, uncomment the following!
+		// @ts-ignore
+		// this.registerEvent(this.app.workspace.on('hover-link', (data) => {
+		// 	console.log(data);
+		// 	const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 	console.log(view?.currentMode === data.hoverParent); // true
+		// }))
+
+		this.registerHoverLinkSource('my-plugin', { // This is the source name that will be used as the "source" attribute in the hover-link event
+			display: 'My plugin',
+			defaultMod: false,
+		});
+
+		this.registerDomEvent(document, 'mouseover', (event) => {
+			if (event.target instanceof HTMLElement && event.target.matches('.inline-title')) {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view?.file) {
+					this.app.workspace.trigger('hover-link', {
+						event,
+						source: 'my-plugin', // the same string as the id that you registered with registerHoverLinkSource
+						hoverParent: view.currentMode,
+						linktext: view.file.path,
+						sourcePath: view.file.path ?? ''
+					});	
+				}
+			}
+		})
+	}
+}
